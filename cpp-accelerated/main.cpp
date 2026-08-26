9cd7c3c2cc78e09e09c9463c15c4ddbde3e21609
Kent Burnett <kentb3217@gmail.com>
2026-08-25 17:41:28 -0700

    Changed from direct initialization to value initialization for all matrices, added initializations for variables.

--- cpp-accelerated/main.cpp
+++ cpp-accelerated/main.cpp
@@ -9,34 +9,21 @@
 int main()
 {
     double counter {0.0};
+    double nodes {static_cast<double>(config.nodes)}; // to prevent narrowing conversions
 
     // velocity of the fluid in x and y directions respectively
-    Matrix u_x (config.nodes, std::vector<double>(config.nodes, 0.0));
-    Matrix u_y (config.nodes, std::vector<double>(config.nodes, 0.0));
-
-    // Phi initialization
-    Matrix phi (config.nodes, std::vector<double>(config.nodes, 0.0));
-
-    // Auxiliary Fields initialization
-    Matrix aux_X(config.nodes, std::vector<double>(config.nodes, 0.0));
-    Matrix aux_Y(config.nodes, std::vector<double>(config.nodes, 0.0));
-
-    // Gradient phi initialization
-    Matrix gradPhi_X(config.nodes, std::vector<double>(config.nodes, 0.0));
-    Matrix gradPhi_Y(config.nodes, std::vector<double>(config.nodes, 0.0));
-
-    // Divergence of the Auxiliary Field initialization
-    Matrix divAuxField(config.nodes, std::vector<double>(config.nodes, 0.0));
+    Matrix u_x {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix u_y {config.nodes, std::vector<double>{nodes, 0.0}};
 
     // Boundary conditions
-    std::vector<double> botx {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> boty {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> upx {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> upy {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> leftx {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> lefty {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> rightx {std::vector<double>(config.nodes, 0.0)};
-    std::vector<double> righty {std::vector<double>(config.nodes, 0.0)};
+    std::vector<double> botx {std::vector<double>{nodes, 0.0}};
+    std::vector<double> boty {std::vector<double>{nodes, 0.0}};
+    std::vector<double> upx {std::vector<double>{nodes, 0.0}};
+    std::vector<double> upy {std::vector<double>{nodes, 0.0}};
+    std::vector<double> leftx {std::vector<double>{nodes, 0.0}};
+    std::vector<double> lefty {std::vector<double>{nodes, 0.0}};
+    std::vector<double> rightx {std::vector<double>{nodes, 0.0}};
+    std::vector<double> righty {std::vector<double>{nodes, 0.0}};
 
     u_x[0] = botx;
     u_y[0] = boty;
@@ -61,6 +48,38 @@ int main()
     double dt {std::min(advective, diffusive)};
 
     // WHILE LOOP
+    double counter {0.0};
+
+    // Initializing partial derivatives, laplacians, auxiliary fields, phi, gradient phi, and divergence of the auxiliary field
+    Matrix phi {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix aux_X {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix aux_Y {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix gradPhi_X {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix gradPhi_Y {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix divAuxField {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix dux_dx {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix dux_dy {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix duy_dx {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix duy_dy {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    Matrix dd_ux {config.nodes, std::vector<double>{nodes, 0.0}};
+    Matrix dd_uy {config.nodes, std::vector<double>{nodes, 0.0}};
+
+    while (counter < config.endTime)
+    {
+        for (int i {1} ; i < (config.nodes - 1) ; ++i)
+        {
+            for (int j {1} ; j < (config.nodes - 1) ; ++j)
+            {
+                
+            }
+        }
+    }
 
     return 0;
 }
